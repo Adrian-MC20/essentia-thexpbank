11f5f9cf3a1b2f0b41e3276c7f113193a3046b2f
Adrian Mihu <adrian.mihu20@icloud.com>
2025-12-17 00:53:46 +0200

    MC version bump to 1.21.11

--- src/main/java/ro/maleficent/essentia/registry/ModDataComponents.java
+++ src/main/java/ro/maleficent/essentia/registry/ModDataComponents.java
@@ -5,7 +5,7 @@ import static ro.maleficent.essentia.Essentia.MOD_ID;
 import net.minecraft.core.Registry;
 import net.minecraft.core.component.DataComponentType;
 import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.Identifier;
 import net.minecraft.util.ExtraCodecs;
 
 public class ModDataComponents {
@@ -14,7 +14,7 @@ public class ModDataComponents {
     public static final DataComponentType<Integer> STORED_XP =
             Registry.register(
                     BuiltInRegistries.DATA_COMPONENT_TYPE,
-                    ResourceLocation.fromNamespaceAndPath(MOD_ID, "stored_xp"),
+                    Identifier.fromNamespaceAndPath(MOD_ID, "stored_xp"),
                     DataComponentType.<Integer>builder()
                             .persistent(ExtraCodecs.NON_NEGATIVE_INT)
                             .build());
